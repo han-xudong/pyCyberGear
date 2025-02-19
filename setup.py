1209fe2361bce29a93ecdc471a472d96417465cc
han-xudong <42986190+han-xudong@users.noreply.github.com>
2025-02-19 15:36:44 +0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycybergear',
-    version='0.1.0',
+    version='1.0.1',
     description='Python library for controlling CyberGear motors',
     author='Xudong Han',
     url='https://github.com/han-xudong/pyCyberGear',
@@ -21,6 +21,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
